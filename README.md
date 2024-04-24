c21b43a4c1d8e9c190e342c6e7fbe45390e2d525
Elgin Beloy <beloy.elgin@gmail.com>
2024-04-23 18:08:30 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+## Directory Tree Printer
+
+This script prints out a file tree starting from a specified path.
+The output includes directories, regular files, and optionally hidden files.
+Files and directories are color-coded for easy identification.
+
+### Usage
+
+Run the script with the following command:
+
+```bash
+python3 file_tree_printer.py --path [path] --exclude_hidden [exclude_hidden] --sleep [sleep] --exclude_names [exclude_names] --verbose [verbose]
+```
+
+or add it as an alias to your `.bashrc`, `.zshrc`, or other `.rc` for 
+quick usage in your cli as the default path is the current directory.
+
+```bash
+alias tree="python3 ~/tree/main.py"
+```
+
+### Arguments
+
+- `--path`: The path to start the tree from. Default is the current directory.
+- `--exclude_hidden`: Exclude .hidden files. Default is False.
+- `--sleep`: Sleep after each directory is printed. Default is False.
+- `--exclude_names`: Exclude files with the names. Provide a space-separated list. Default is an empty list.
+- `--verbose`: Verbose output like if hidden files were omitted. Default is False.
+
+### Output
+
+The script prints out the file tree, with directories and files color-coded. If `--verbose` is set to True, it also prints out messages about hidden and excluded files.
+
+### Dependencies
+
+This script requires the `os`, `argparse`, `time`, and `termcolor` modules.
---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+import time
+from termcolor import colored
+
+def print_files_in_path(
+  path="/", indent=0, exclude_hidden=False, sleep=False, exclude_names=[]):
+  for file in os.listdir(path):
+    if file.startswith("."):
+      if exclude_hidden:
+        hidden_messages.append("Not showing hidden file: " + file)
+      else:
+        print((" "*indent) + colored(file, "blue"))
+      continue
+
+    if file in exclude_names:
+      hidden_messages.append("Not showing excluded file: " + file)
+      continue
+
+
+    file_path = f"{path}/{file}"
+    if file.startswith("_") and not os.path.isdir(file_path):
+      print((" "*indent) + colored(file, "yellow"))
+      continue
+
+    if os.path.isdir(file_path):
+      if sleep:
+        time.sleep(0.5)
+      print((" "*indent) + colored(file, "green", attrs=["bold", "reverse"]))
+      print_files_in_path(
+        path=file_path,
+        indent=indent+4,
+        exclude_hidden=exclude_hidden,
+        sleep=sleep,
+        exclude_names=exclude_names)
+    else:
+      print((" "*indent) + file)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", default="./", help="The path to start the tree from")
+parser.add_argument("--exclude_hidden", action="store_true", default=False, help="Exclude .hidden files")
+parser.add_argument("--sleep", action="store_true", default=False, help="Sleep after each directory is printed.")
+parser.add_argument("--exclude_names", nargs="*", default=[], help="Exclude files with the names")
+parser.add_argument("--verbose", action="store_true", help="Verbose output like hidden files")
+args = parser.parse_args()
+
+hidden_messages = []
+print(f"Running Tree: verbose={args.verbose} " + \
+  f"| exclude_hidden={args.exclude_hidden} " + \
+  f"| exclude_names={args.exclude_names}")
+print_files_in_path(
+  args.path,
+  exclude_hidden=args.exclude_hidden,
+  sleep=args.sleep,
+  exclude_names=args.exclude_names)
+
+if args.verbose:
+  for msg in hidden_messages:
+    print(colored(msg, "red", attrs=["bold"]))
